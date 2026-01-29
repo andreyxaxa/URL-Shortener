ea25d8662bdc90640fd66a6c3bb2f046826cd1a3
Petrov Andrey <4ndrew1337@mail.ru>
2026-01-29 21:58:52 +0300

    chore: add config

---
+++ config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/caarlos0/env/v11"
+)
+
+type (
+	Config struct {
+		HTTP  HTTP
+		Log   Log
+		PG    PG
+		Redis Redis
+	}
+
+	HTTP struct {
+		Port string `env:"HTTP_PORT,required"`
+	}
+
+	Log struct {
+		Level string `env:"LOG_LEVEL,required"`
+	}
+
+	PG struct {
+		URL     string `env:"PG_URL,required"`
+		PoolMax int    `env:"PG_POOL_MAX,required"`
+	}
+
+	Redis struct {
+		Addr        string `env:"REDIS_ADDR,required"`
+		DB          int    `env:"REDIS_DB,required"`
+		User        string `env:"REDIS_USER"`
+		Password    string `env:"REDIS_PASSWORD"`
+		DialTimeout int    `env:"REDIS_DIAL_TIMEOUT"`
+		Timeout     int    `env:"REDIS_TIMEOUT"`
+	}
+)
+
+func New() (*Config, error) {
+	cfg := &Config{}
+
+	if err := env.Parse(cfg); err != nil {
+		return nil, fmt.Errorf("config error: %v", err)
+	}
+
+	return cfg, nil
+}
